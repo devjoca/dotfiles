c27c3c99435b93968d717aae36cfe16cc9597c91
Joca <jpereyraleon@gmail.com>
2016-12-03 15:07:05 -0500

    add more composer pckgs

--- install.sh
+++ install.sh
@@ -8,3 +8,20 @@ brew cask install spotify
 
 #install nylas mail client
 brew cask install nylas-n1
+
+#install iterm2
+brew cask install iterm2
+
+#install zsh
+brew install zsh zsh-completions
+
+
+#install valet with php and mariadb
+brew install homebrew/php/php70
+brew install mariadb
+brew install composer
+composer require consolidation/cgr
+cgr laravel/valet
+cgr laravel/installer
+
+
