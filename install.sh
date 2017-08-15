1c79a3ece25626db169bc4b01fd04d518077095d
Joca <jpereyraleon@gmail.com>
2017-08-15 11:48:03 -0500

    update preferences for prezto and vim

--- install.sh
+++ install.sh
@@ -3,25 +3,18 @@
 #install misc apps
 brew cask install telegram
 brew cask install spotify
-brew cask install spotify-notifications
-brew cask install nylas-n1
 
 #install iterm2
 brew cask install iterm2
-
-#install zsh and oh my zsh
-brew install zsh zsh-completions
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 
 #node and yarn
 brew install node
 brew install yarn
 
 #install valet with php and mariadb
-brew install homebrew/php/php70
-brew install mariadb
+brew install homebrew/php/php71
 brew install composer
+brew install mysql
 composer global require consolidation/cgr
 cgr laravel/valet
 cgr laravel/installer
